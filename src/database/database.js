cbd1d7a93a9e7ef874b49a8c5859a55b0d958a91
ET\patty.castro <Ende.3030>
2023-09-15 20:57:33 -0400

    segundo commit

--- src/database/database.js
+++ src/database/database.js
@@ -1,21 +1,23 @@
 import { Sequelize } from 'sequelize';
-import 'dotenv/config';
+import dotenv from "dotenv";
+dotenv.config();
 
 
-const NOMBRE_BASE = process.env.NOMBRE_BASE;
-const USERNAME = process.env.USERNAME;
-const PASSWORD = process.env.PASSWORD;
-const HOST = 'berry.db.elephantsql.com'; //process.env.HOST;
-const MOTOR = process.env.MOTOR;
+
+ const NOMBRE_BASE = process.env.NOMBRE_BASE;
+ const USERNAME = process.env.USERNAME;
+ const PASSWORD = process.env.PASSWORD;
+ const HOST = 'berry.db.elephantsql.com'; //process.env.HOST;
+ const MOTOR = process.env.MOTOR;
 
 
 export const sequelize = new Sequelize(
-  process.env.NOMBRE_BASE, // db name
-  process.env.USERNAME, // username
-  process.env.PASSWORD, // password
+  process.env.DB_NAME, // db name
+  process.env.DB_USERNAME, // username
+  process.env.DB_PASSWORD, // password
   {
-    host: process.env.HOST,
-    dialect: process.env.MOTOR,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
   }
 );
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import  express  from 'express';
+import dotenv from "dotenv";
 import app from './app.js';
 import { sequelize } from './database/database.js';
-import 'dotenv/config';
 
+dotenv.config();
 
-async function main() {
-    console.clear
+async function main(){ 
+    console.clear();
     await sequelize.sync({ force: true });
     const PORT = process.env.PORT;
     app.listen(PORT);
